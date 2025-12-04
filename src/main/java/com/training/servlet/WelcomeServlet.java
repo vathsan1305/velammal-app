2cbaf69ce910fa7825f5343f6c2a03e53d4ea105
vathsan1305 <vathsan1305@gmail.com>
2025-12-04 12:15:22 +0530

    updates welcome code

--- src/main/java/com/training/servlet/WelcomeServlet.java
+++ src/main/java/com/training/servlet/WelcomeServlet.java
@@ -21,43 +21,39 @@ import impl.StudentDaoImplementation;
 public class WelcomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
 	@SuppressWarnings("rawtypes")
 	private Dao dao;
-	
-    public WelcomeServlet() throws SQLException {
-        super();
-        // TODO Auto-generated constructor stub
-        dao = new StudentDaoImplementation(MySqlSConnection.getMySqlConnection());
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// ❗ Constructor must NOT open database connection
+	public WelcomeServlet() {
+		super();
+	}
+
+	// ❗ Correct place to initialize DAO
+	@Override
+	public void init() throws ServletException {
+		try {
+			dao = new StudentDaoImplementation(MySqlSConnection.getMySqlConnection());
+		} catch (SQLException e) {
+			throw new ServletException(e);
+		}
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("<h1 style='text-align:center'>Velammal Main School</h1>");//can send response
-		
+
 		request.setAttribute("title","Velammal Main School" );
 		
 		try {
 			request.setAttribute("list",dao.findAll());
 		} catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
 		dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response); //can receive response
+		doGet(request, response);
 	}
-
 }
